b5cc093fef8ee574f32e89efed44921e075d7ffa
Lorena πacente <149822592+LorenaPiacente@users.noreply.github.com>
2026-03-20 14:06:55 -0300

    choire: add initial plant mock data

---
+++ src/app/api/data.js
@@ -0,0 +1,16 @@
+export const bibliotecaPlantas = [
+    {
+        id: 1,
+        nome: "Canteiro de Lavanda",
+        nomeCientifico: "Lavandula angustifolia",
+        uso: "Bordadura / Ornamental",
+        clima: "Sol Pleno"
+    },
+    {
+        id: 2,
+        nome: "Palmeira Raphis",
+        nomeCientifico: "Raphis excelsa",
+        uso: "Interiores / Vasos",
+        clima: "Meia Sombra"
+    },
+];
